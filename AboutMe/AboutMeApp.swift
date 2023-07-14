59b1a9b9b744eeece0c18a7539f62742dda34edc
Dagmar <beckeldagmar@gmail.com>
2023-07-14 15:49:00 -0500

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by GoldenCalfCompanyMacPro2 on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
